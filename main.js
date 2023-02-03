60d0b39f183c3eec06c6ddb3ee08fd8e9cb1b831
sulaymonkhon1238 <sulaymonkhon2222@gmail.com>
2023-02-03 12:08:36 +0500

    first js commit

---
+++ main.js
@@ -0,0 +1,59 @@
+//  var a = 20
+//  var b = 10
+
+// console.log(a)
+// console.log(b)
+// let hello = 'Hello world!';
+
+// let message;
+
+// // copy 'Hello world' from hello into message
+// message = hello;
+
+// // now two variables hold the same data
+// alert(hello); // Hello world!
+// alert(message); // Hello world
+// let isGreater = 4 > 1;
+
+// alert( isGreater ); // true (the comparison result is "yes")
+// let ism = "sulaymon"
+// let age = 22
+
+// alert( `salom ${ism}` ); // ?
+
+// prompt( `${ism} ${age} yoshda` ); // ?
+
+// alert( `hello ${name}` ); // ?
+// open console to see
+// for (let i = 1; i < 81; i++) {
+//     console.log("value,", i);
+//   }
+//   let age = prompt ("how old are you ?" , 22)
+//   alert ('you are ${age} years old!')
+// alert ("welcome")
+// let ism = prompt("ismingizni kiriting");
+// let yosh = prompt("yoshingizni kiriting");
+// let ish = prompt("kasbingizni kiriting");
+
+// console.log(`
+// User info:
+// name: ${ism} 
+// age: ${age} 
+// jobx: ${ish} 
+// `)
+prompt("Jinsingizni kiriting");
+let erkak = "Siz o'g'il bolasiz";
+let ayol = "Siz qiz bolasiz";
+let yosh = prompt("Yoshingizni kiriting");
+ if ( yosh <= 18 && erkak ){
+   console.log('siz yosh ogil bolasiz oqishingiz kerak')
+ }
+ else if ( yosh > 18 && erkak){
+   console.log("siz ogil bolasiz ishlasangiz boladi")
+ }
+ else if ( yosh <= 18 && ayol){
+   console.log("Siz yosh qiz bolasiz oqishingiz kerak")
+
+ } else if ( yosh > 18 && ayol){
+   console.log('siz katta qizsiz ishlasangiz boladi')
+  }
